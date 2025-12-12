c8f6e88ec680062a8a5f497929a914fdb10c8382
Edson Marco <edin2008@yahoo.com.br>
2025-12-12 16:06:16 -0300

    Criando tela Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,30 @@
 import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import bannerImage from "../../assets/banner.png";
+import { Container, TextContent, Title, TitleHighlight } from "./style.s";
 
 const Home = () => {
     return(<>
     <Header />
+    <Container>
+        <div>
+            <Title>
+                <TitleHighlight>
+                    Implemente
+                    <br />
+                </TitleHighlight>
+                O seu futuro global agora
+            </Title>
+            <TextContent>
+                Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare o seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+            </TextContent>
+            <Button title="Começar agora" variant="secundary" onClick={() => null} />
+        </div>
+        <div>
+            <img src= {bannerImage } alt="Imagem principal" />
+        </div>
+    </Container>
     </>)
 }
 
