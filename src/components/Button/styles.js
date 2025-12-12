957b3b76ae258ff728e7c6ef289febd932e04ec0
Edson Marco <edin2008@yahoo.com.br>
2025-12-12 15:17:19 -0300

    inicializando projeto e adicionando header

---
+++ src/components/Button/styles.js
@@ -0,0 +1,31 @@
+import styled, { css } from "styled-components";
+
+export const ButtonContainer = styled.button`
+    background: #565656;
+    border-radius: 22px;
+    position: relative;
+
+    color: #FFF;
+    padding: 2px 12px;
+    width: 100%;
+
+    //criando uma propriedade com estilo diferente caso atenda as condições
+    ${({variant}) => variant !== "primary"  && css`
+        min-width: 167px;
+        height: 33px;
+        
+        background: #e4105D;
+
+        &::after {
+            content: '',
+            position: absolute;
+            border: 1px solid #E4105D;
+            top: -5px;
+            left: -6px;
+            width: calc(100% + 10px);
+            height: calc(100% + 10px);
+            border-radius: 22px;
+        }
+    `}
+
+`
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+
+const Home = () => {
+    return(<>
+    <Header />
+    </>)
+}
+
+export { Home };
