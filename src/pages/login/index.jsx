a9475b8b8e9d0840aed8e43d68f54a3b57461e2e
Edson Marco <edin2008@yahoo.com.br>
2025-12-17 13:39:56 -0300

    Alterando a Tela Login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom";
+import { MdEmail, MdLock } from 'react-icons/md'
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
+import { Container, Title, Column, Row, CriarText, EsqueciText, SubTitleLogin, TitleLogin, Wrapper } from './styles';
 
 const Login = () => {
     return(<>
-    <h1>Login</h1>
-    <Link to={'/'} >Voltar para Home</Link>
+    <Header />
+    <Container>
+        <Column>
+            <Title>
+                A plataforma para você aprender com experts, dominar as principais tecnologias 
+                e entrar mais rápido nas empresas mais desejadas.
+            </Title>
+        </Column>
+        <Column>
+            <Wrapper>
+                <TitleLogin>Faça seu cadastro!</TitleLogin>
+                <SubTitleLogin>Faça seu Login e make the change.</SubTitleLogin>
+                <form>
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} />
+                    <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
+                    <Button title="Entrar" variant="secondary" />
+                </form>
+                <Row>
+                    <EsqueciText>Esqueci minha senha</EsqueciText>
+                    <CriarText>Criar Conta</CriarText>
+                </Row>
+            </Wrapper>
+        </Column>
+    </Container>
     </>)
 }
 
